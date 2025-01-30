4121b27b77f6e098e9ed103d6d544a0aed9aed85
Bunyoddff <bkhusanov444@gmail.com>
2025-01-30 17:00:48 +0500

    add 17 student to student base

--- studentbase.py
+++ studentbase.py
@@ -1,21 +1,122 @@
-studentbase={
-
-'user_1':[{
-        'name':'Sayfulloh',
-        'age':19,
-        'surname':'Ergashev',
-        'job':'student'
-}],
-'user_2':[{
-        'name':'Bunyod',
-        'age':24,
-        'surname':'Xusanov',
-        'job':'student'
-}],
-'user_3':[{
-        'name':'Abdumannon',
-        'age':25,
-        'surname':'Halimov',
-        'job':'engineer'
-}]
+studentbase=users = {
+    'user_1': [{
+        'name': 'Sayfulloh',
+        'age': 19,
+        'surname': 'Ergashev',
+        'job': 'student'
+    }],
+    'user_2': [{
+        'name': 'Bunyod',
+        'age': 24,
+        'surname': 'Xusanov',
+        'job': 'student'
+    }],
+    'user_3': [{
+        'name': 'Abdumannon',
+        'age': 25,
+        'surname': 'Halimov',
+        'job': 'engineer'
+    }],
+    'user_4': [{
+        'name': 'Umid',
+        'age': 22,
+        'surname': 'Karimov',
+        'job': 'developer'
+    }],
+    'user_5': [{
+        'name': 'Shakhzod',
+        'age': 20,
+        'surname': 'Tursunov',
+        'job': 'student'
+    }],
+    'user_6': [{
+        'name': 'Diyor',
+        'age': 27,
+        'surname': 'Sattorov',
+        'job': 'data scientist'
+    }],
+    'user_7': [{
+        'name': 'Azizbek',
+        'age': 21,
+        'surname': 'Norbekov',
+        'job': 'designer'
+    }],
+    'user_8': [{
+        'name': 'Otabek',
+        'age': 30,
+        'surname': 'Rustamov',
+        'job': 'doctor'
+    }],
+    'user_9': [{
+        'name': 'Ibrohim',
+        'age': 23,
+        'surname': 'Gafurov',
+        'job': 'teacher'
+    }],
+    'user_10': [{
+        'name': 'Kamron',
+        'age': 26,
+        'surname': 'Saidov',
+        'job': 'engineer'
+    }],
+    'user_11': [{
+        'name': 'Nodir',
+        'age': 28,
+        'surname': 'Mahmudov',
+        'job': 'business analyst'
+    }],
+    'user_12': [{
+        'name': 'Sarvar',
+        'age': 24,
+        'surname': 'Komilov',
+        'job': 'researcher'
+    }],
+    'user_13': [{
+        'name': 'Farrukh',
+        'age': 22,
+        'surname': 'Ismoilov',
+        'job': 'developer'
+    }],
+    'user_14': [{
+        'name': 'Jasur',
+        'age': 29,
+        'surname': 'Sharipov',
+        'job': 'architect'
+    }],
+    'user_15': [{
+        'name': 'Alisher',
+        'age': 31,
+        'surname': 'Rakhimov',
+        'job': 'scientist'
+    }],
+    'user_16': [{
+        'name': 'Rustam',
+        'age': 27,
+        'surname': 'Bekmurodov',
+        'job': 'lawyer'
+    }],
+    'user_17': [{
+        'name': 'Temur',
+        'age': 19,
+        'surname': 'Niyozov',
+        'job': 'intern'
+    }],
+    'user_18': [{
+        'name': 'Muhammad',
+        'age': 25,
+        'surname': 'Soliev',
+        'job': 'banker'
+    }],
+    'user_19': [{
+        'name': 'Zafar',
+        'age': 32,
+        'surname': 'Khodjaev',
+        'job': 'manager'
+    }],
+    'user_20': [{
+        'name': 'Sherzod',
+        'age': 21,
+        'surname': 'Xalilov',
+        'job': 'mechanic'
+    }]
 }
