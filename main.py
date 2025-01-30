70184bf5ad125cf451ebd6d2a5d94b42995d1c9d
Bunyoddff <bkhusanov444@gmail.com>
2025-01-30 16:50:11 +0500

    add three student

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tinydb import TinyDB, Query
-
+from studentbase import studentbase
 # Create or connect to the database
+st=TinyDB('student.json')
 db = TinyDB('students.json')
-
-# Reference the default table
-students_table = db.table('students')
+for i in studentbase:
+    for item in studentbase[i]:
+        st.insert(item)
